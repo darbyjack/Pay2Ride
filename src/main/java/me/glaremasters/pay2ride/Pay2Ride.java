51a0eb9f0b216746067e8414ec77187d79a11fe2
darbyjack <admin@glaremasters.me>
2018-01-31 22:39:03 -0600

    Base files

---
+++ src/main/java/me/glaremasters/pay2ride/Pay2Ride.java
@@ -0,0 +1,20 @@
+package me.glaremasters.pay2ride;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by GlareMasters on 1/31/2018.
+ */
+public class Pay2Ride extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
